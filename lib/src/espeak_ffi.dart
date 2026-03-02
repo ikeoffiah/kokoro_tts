0b53880edb7df247b15a6c502837c78dc9ca20cb
PIUS IKEOFFIAH <piuspio12@gmail.com>
2026-03-02 11:06:16 +0000

    feat: Completed the espeaks C to dart bridge using dart ffi

--- lib/src/espeak_ffi.dart
+++ lib/src/espeak_ffi.dart
@@ -2,20 +2,20 @@ import 'dart:ffi';
 import 'dart:io';
 import 'package:ffi/ffi.dart';
 
-typedef _EspeakIntializeC =
+typedef _EspeakInitializeC =
     Int32 Function(
       Int32 output,
       Int32 buflength,
-      Pointer<Char> path,
+      Pointer<Utf8> path,
       Int32 options,
     );
 
 typedef _EspeakInitializeDart =
-    int Function(int output, int buflength, String path, int options);
+    int Function(int output, int buflength, Pointer<Utf8> path, int options);
 
-typedef _EspeakSetVoiceByNameC = Int32 Function(Pointer<Char> name);
+typedef _EspeakSetVoiceByNameC = Int32 Function(Pointer<Utf8> name);
 
-typedef _EspeakSetVoiceByNameDart = int Function(String name);
+typedef _EspeakSetVoiceByNameDart = int Function(Pointer<Utf8> name);
 
 typedef _EspeakTextToPhonemesC =
     Pointer<Utf8> Function(
@@ -25,7 +25,7 @@ typedef _EspeakTextToPhonemesC =
     );
 
 typedef _EspeakTextToPhonemesDart =
-    String Function(
+    Pointer<Utf8> Function(
       Pointer<Pointer<Void>> textptr,
       int textmode,
       int phonememode,
@@ -61,6 +61,79 @@ class EspeakFfi {
       throw UnsupportedError(
         'Unsupported platform: ${Platform.operatingSystem}',
       );
+    }
+
+    _espeakInitialize = _lib!
+        .lookupFunction<_EspeakInitializeC, _EspeakInitializeDart>(
+          'espeak_Initialize',
+        );
+    _espeakSetVoiceByName = _lib!
+        .lookupFunction<_EspeakSetVoiceByNameC, _EspeakSetVoiceByNameDart>(
+          'espeak_SetVoiceByName',
+        );
+    _espeakTextToPhonemes = _lib!
+        .lookupFunction<_EspeakTextToPhonemesC, _EspeakTextToPhonemesDart>(
+          'espeak_TextToPhonemes',
+        );
+    _espeakTerminate = _lib!
+        .lookupFunction<_EspeakTerminateC, _EspeakTerminateDart>(
+          'espeak_Terminate',
+        );
+    _loaded = true;
+  }
+
+  // Initialize the espeak-ng library with the path to the directory cotaing the espeak-ng-data directory
+  void init({required String dataPath}) {
+    final pathPtr = dataPath.toNativeUtf8();
+    try {
+      // AUDIO_OUTPUT_RETRIEVAL = 0x002, DONT_EXIT = 0X8000
+      final result = _espeakInitialize!(0x002, 0, pathPtr, 0x8000);
+      if (result < 0) {
+        throw Exception('Failed to initialize espeak-ng: $result');
+      }
+      final voicePtr = 'en-us'.toNativeUtf8();
+      try {
+        final result = _espeakSetVoiceByName!(voicePtr);
+        if (result < 0) {
+          throw Exception('Failed to set voice: $result');
+        }
+      } finally {
+        calloc.free(voicePtr);
+      }
+      _ready = true;
+    } finally {
+      calloc.free(pathPtr);
+    }
+  }
+
+  String phonemize(String text) {
+    if (!_ready) throw Exception('Espeak-ng not initialized');
+    final textPtr = text.toNativeUtf8();
+    final ptrToPtr = calloc<Pointer<Void>>();
+    ptrToPtr.value = textPtr.cast<Void>();
+    try {
+      final buffer = StringBuffer();
+
+      while (true) {
+        // textmode = 1 (UTF-8), phonemode = 0x02 (IPA)
+        final phonemesPtr = _espeakTextToPhonemes!(ptrToPtr, 1, 0x02);
+        if (phonemesPtr == nullptr) break;
+        final phonemes = phonemesPtr.toDartString();
+        if (phonemes.isEmpty) break;
+        buffer.write(phonemes);
+        buffer.write(' ');
+      }
+      return buffer.toString().trim();
+    } finally {
+      calloc.free(ptrToPtr);
+      calloc.free(textPtr);
+    }
+  }
+
+  void dispose() {
+    if (!_ready) {
+      _espeakTerminate!.call();
+      _ready = false;
     }
   }
 }
