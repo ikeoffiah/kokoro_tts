6e4f590bcd40e6fbffd67bf422708b77738772dd
PIUS IKEOFFIAH <piuspio12@gmail.com>
2026-03-03 01:06:37 +0000

    feat: Added configurations for ios

---
+++ ios/Classes/espeak-ng/espeak_bridge.h
@@ -0,0 +1,6 @@
+#ifndef ESPEAK_BRIDGE_H
+#define ESPEAK_BRIDGE_H
+
+void kitten_tts_force_link_espeak(void);
+
+#endif
--- test/flutter_kokoro_tts_test.dart
+++ test/flutter_kokoro_tts_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_kokoro_tts/flutter_kokoro_tts.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('KokoroTts exposes availableVoices and sampleRate', () {
+    final tts = KokoroTts();
+    expect(tts.availableVoices, isNotEmpty);
+    expect(tts.availableVoices, contains('Default'));
+    expect(tts.sampleRate, 24000);
+  });
+
+  test('kokoroVoices is exported and matches availableVoices', () {
+    final tts = KokoroTts();
+    expect(tts.availableVoices, equals(kokoroVoices));
   });
 }
